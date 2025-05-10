d4d54b01f26abfa121ace5063326185d778d92a6
41ucet <41ucet2020@gmail.com>
2025-05-10 15:23:13 +0530

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/:path*',
+            headers: [
+              { key: 'X-Frame-Options', value: 'ALLOWALL' },
+              { key: 'Content-Security-Policy', value: "frame-ancestors 'self' *" },
+            ],
+          },
+        ];
+      },
+};
 
 export default nextConfig;
